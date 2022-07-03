75c1878772e5d266fe4851d87784de39132359cf
Santi <xxsmating@gmail.com>
2022-07-03 06:52:16 -0300

    primeraParte

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,77 @@
-let product = "";
-let price = 0;
-let iva1 = 0;
-let descuentoS = 0;
-let iThink = "";
-let ivaExterno = 0;
-let cantProductos = 0;
-let additionPrice = 0;
-let limit = 15000;
-const init = (product,price) =>{
-    return "El nombre del producto es: "+product+"\n y el precio del mismo es: $"+price;
+const iva=(nameS,price)=>{
+    let iva = 0.21;
+    let result = (price*iva)+price;
+    console.log("The name of the product is: "+nameS+" and the price of this is: u$d"+price);
 }
 
-const numero = numero =>{
-    return numero;
-}
-asdasdasd
-const iva = (iva1,price) => {
-    iva1 = ((iva1/100)*parseInt(price))+parseInt(price);
-    ivaExterno = iva1;
-    return "El precio total con IVA es: "+iva1;
-}
-const descuento = (descuento,iva1) =>{
-    descuento = iva1-((iva1)*(descuento/100));
-    additionPrice += descuento;
-    return "El precio final con el descuento es: "+descuento;
-}
-const postBucle = (cantProductos, additionPrice, limit) =>{
-    alert("Se han registrado "+cantProductos+ " productos que ha comprado");
-    alert("Ha gastado en total: "+additionPrice);
-    if (additionPrice > limit){
-        alert("Ha gastado más de lo que tiene, se procederá a una deuda y se cobrará el doble de lo que gastó: $"+(additionPrice*2));
+const productsAMD = [
+    {nameS: "Ryzen 3 1200", price: 1100},
+    {nameS: "Ryzen 3 2200g", price: 1350},
+    {nameS: "Ryzen 5 2400g", price: 1800},
+    {nameS: "Ryzen 5 3600", price: 2500},
+    {nameS: "Ryzen 7 3700x", price: 6500},
+    {nameS: "Ryzen 5 5600x", price: 8000},
+    {nameS: "Ryzen 9 3900x", price: 9000},
+    {nameS: "Ryzen 9 3990xt", price: 13370}
+];
+const products = [
+    {nameS:"INTEL", stock: true},
+    {nameS:"AMD", stock: true},
+    {nameS:"NVIDIA", stock: false}
+];
+const productsIntel = [
+    {nameS: "i3 9100F", price: 2000},
+    {nameS: "i5 9400F", price: 2500},
+    {nameS: "i7 7700K", price: 3000},
+    {nameS: "i9 9900KF", price: 5000},
+    {nameS: "i5 10600K", price: 7635},
+    {nameS: "i5 12400F", price: 9000},
+    {nameS: "i7 12700KF", price: 10000},
+    {nameS: "i9 12900F", price: 18000},
+    {nameS: "i9 12900K", price: 30000}
+];
+class Product{
+    constructor(names, price){
+        this.names = names;
+        this.price = price;
+        this.sold = false;
     }
+    sold(){
+        this.sold = true;
+    }
+};
+let amdProducts;
+let nameProduct;
+let priceProduct;
+alert("Welcome to TecnoStore");
+alert("Here you can watch the best prices for components of PC in the market");
+nameProduct = prompt("First, what are you searching? AMD/Intel/Nvidia").toUpperCase();
+const array = products.map((elementProcessors) => elementProcessors.nameS);
+const array2 = products.map(elementProcessors => elementProcessors.stock);
+console.log(array);
+while(array.indexOf(nameProduct) == -1){
+    nameProduct = prompt("Again, what are you searching?").toUpperCase();
 }
-do{
-    let contador = prompt("Digite cantidad de productos");
-    for (let i = 0; i<numero(contador); i++){
-        product = prompt("Digite el nombre del producto (o presione ESC para salir)").toUpperCase();
-        if (product == "ESC"){
-            break;
+if (array.indexOf(nameProduct)!=-1){
+    if (array2[array.indexOf(nameProduct)] == true){
+        alert("Oh, great! We have components of this type");
+        if (array2[array.indexOf(nameProduct)] == 1 ){
+            for (const nameProducts of productsAMD){
+                alert("Here are the products of AMD: "+nameProducts.nameS);
+                console.log(nameProducts.nameS);
+            }
+            nameProduct = prompt("What do you want to buy?");
+            const array3 = productsAMD.map (elementProcessors => elementProcessors.nameS);
+            console.log(array3.indexOf(nameProduct));
+            if (array3.indexOf(nameProduct) != -1){
+                alert("Ok, you are buying: "+array3[array3.indexOf(nameProduct)]);
+                console.log(array3);
+                array3.splice(array3.indexOf(nameProduct),1);
+                console.log(array3);
+            }
         }
-        price = prompt("Digite el precio del producto").toUpperCase();
-        if (price == "ESC"){
-            break;
-        }
-        parseInt(price);
-        console.log(init(product,price));
-        iva1 = parseInt(prompt("Digite el iva"));
-        console.log(iva(iva1,price));
-        descuentoS = parseInt(prompt("Digite el porcentaje del descuento"));
-        console.log(descuento(descuentoS, ivaExterno));
-        cantProductos++;
     }
-    if ((price != "ESC") && (product != "ESC")){
-        iThink = prompt("Desea continuar? Si/No").toUpperCase();
-    }else{
-        break;
+    else{
+        alert("Sorry, we haven't components of this type. Comeback later :(");
     }
-}while((product != "ESC") && (price != null) &&(iThink == "SI"));
-postBucle(cantProductos, additionPrice, limit);
+}
